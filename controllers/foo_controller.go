853e091385d05e891a204a90e768cfa3bbf68882
agocover <inukaitai4@gmail.com>
2020-01-06 23:24:39 +0900

    second commit

--- controllers/foo_controller.go
+++ controllers/foo_controller.go
@@ -17,6 +17,7 @@ package controllers
 
 import (
 	"context"
+	"github.com/prometheus/common/log"
 
 	"github.com/go-logr/logr"
 	appsv1 "k8s.io/api/apps/v1"
@@ -44,15 +45,40 @@ type FooReconciler struct {
 // +kubenuilder:rbac:groups="",reources=events,verbs=create;patch
 
 func (r *FooReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
-	_ = context.Background()
-	_ = r.Log.WithValues("foo", req.NamespacedName)
+	ctx := context.Background()
+	log := r.Log.WithValues("foo", req.NamespacedName)
 
 	// your logic here
+	var foo samplecontrollerv1alpha1.Foo
+	log.Info("fetching Foo Resource")
+	if err := r.Get(ctx, req.NamespacedName, &foo); err != nil {
+		log.Error(err, "unable to fetch Foo")
+		return ctrl.Result{}, client.IgnoreNotFound(err)
+	}
 
 	return ctrl.Result{}, nil
 }
 
+var (
+	deploymentOwnerKey = ".metadata.controller"
+	apiGVStr = samplecontrollerv1alpha1.GroupVersion.String()
+)
+
 func (r *FooReconciler) SetupWithManager(mgr ctrl.Manager) error {
+
+	if err := mgr.GetFieldIndexer().IndexField(&appsv1.Deployment{}, deploymentOwnerKey, func(rawObj runtime.Object) []string {
+		deployment := rawObj.(*appsv1.Deployment)
+		owner := metav1.GetControllerOf(deployment)
+		if owner == nil {
+			return nil
+		}
+		if owner.APIVersion != apiGVStr || owner.Kind != "Foo" {
+			return nil
+		}
+		return []string{owner.Name}
+	}); err != nil {
+		return err
+	}
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&samplecontrollerv1alpha1.Foo{}).
 		Complete(r)
